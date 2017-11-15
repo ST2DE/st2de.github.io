fa46e8815b7296555e86418601a16340ff82aca2
Negaihoshi <e222et@gmail.com>
2017-11-15 22:22:41 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -14,14 +14,59 @@
 
     <section>
       <v-layout column wrap class="my-5" align-center>
-        <v-flex xs12 sm4 class="my-3">
-          <div class="text-xs-center">
-            <h2 class="headline">The best way to start developing</h2>
-            <span class="subheading">
+        <section>
+          <v-container grid-list-xl>
+            <v-layout row wrap justify-center class="my-5">
+              <v-flex xs12 sm6>
+                <v-card class="elevation-0 transparent">
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline blue--text">前言</div>
+                  </v-card-title>
+                  <v-card-text>
+                    <span class="subheading">這是一個全民寫程式的時代，成為一個工程師是許多畢業生或是轉職的選項。網路上雖然有許多學習資源，但卻未必人人都能在學習後立即投入網站開發。實際的情況是「第一次學寫程式就失敗」，一個人自學其實是會遇到很多撞牆期的。像是：</span><br>
+                    <!-- <br> -->
+                    <v-layout row wrap justify-center class="my-5">
+                      <ul class="subheading">
+                        <li>網路上資源好多不知道從哪邊開始？</li>
+                        <li>卡關了怎麼辦找不到人問問題？</li>
+                        <li>學著基礎的程式後不知道可以幹嘛？</li>
+                        <li>不清楚團隊合作或實務開發上該如何進行？</li>
+                      </ul>
+                    </v-layout>
+                    <!-- <br> -->
+                    <span class="subheading">我們認為，成為一個網站開發的自學者，不應該是一個人默默地前進。更需要的是，有一些相關經驗的前輩能夠適時地提供指引與協助。我們希望透過資深工程師帶領新手入門，讓熱情的學員、初學者也可以踏入技術的殿堂，達到技術傳承與知識延續。</span>
 
-            </span>
-          </div>
-        </v-flex>
+                  </v-card-text>
+                </v-card>
+                <v-card class="elevation-0 transparent">
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline blue--text">活動對象</div>
+                  </v-card-title>
+                  <v-card-text class="layout justify-center">
+                    <span class="subheading">在學學生，且年齡為 26 歲（含）以下</span><br>
+                  </v-card-text>
+                </v-card>
+                <v-card class="elevation-0 transparent">
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline blue--text">活動時間</div>
+                  </v-card-title>
+                  <v-card-text class="layout justify-center">
+                    <span class="subheading">2017/12/1 ~ 2018/1/31</span><br>
+                  </v-card-text>
+                </v-card>
+                <v-card class="elevation-0 transparent">
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline blue--text">活動參與細節</div>
+                  </v-card-title>
+                  <v-card-text class="layout justify-center">
+                    <span class="subheading"><a href="https://hackmd.io/s/Hy8CAOOyf#">ST2DE 學生開發者養成計畫第一梯次</a></span><br>
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </section>
+
         <v-flex xs12>
           <v-container grid-list-xl>
             <v-layout row wrap align-center>
@@ -73,33 +118,33 @@
       </v-layout>
     </section>
 
-    <section>
-      <v-parallax src="/taiwan-1411879_1920.jpg" height="600">
-        <v-layout column align-center justify-center class="black--text">
-          <v-card class="elevation-2" style="opacity:0.9;">
-            <v-card-title>
-              <div>
-                <span class="light-blue--text">第一階段</span>
-              </div>
-            </v-card-title>
-            <v-card-text>
-              <ol>
-                <li>報名 KKTIX（注意：需註冊 KKTIX 帳號，後續才能進行修改）</li>
-                <li>開始練習 <a href="https://www.freecodecamp.org/">freeCodeCamp</a> 線上課程</li>
-                <ul>
-                  <li>HTML5 and CSS</li>
-                  <li>Basic JavaScript</li>
-                  <li>Git</li>
-                  <li>Node.js and Express.js</li>
-                </ul>
-                <li>加入社團，遇到問題可以在<a href="https://www.facebook.com/groups/293538921163334">社團</a>裡面討論</li>
-                <li>完成練習，更新 kktix 資料</li>
-                <li>等待第二階段通知</li>
-              </ol>
-            </v-card-text>
-          </v-card>
-        </v-layout>
-      </v-parallax>
-    </section>
+    <!-- <section>
+    <v-parallax src="/taiwan-1411879_1920.jpg" height="600">
+    <v-layout column align-center justify-center class="black--text">
+    <v-card class="elevation-2" style="opacity:0.9;">
+    <v-card-title>
+    <div>
+    <span class="light-blue--text">第一階段</span>
   </div>
+</v-card-title>
+<v-card-text>
+<ol>
+<li>報名 KKTIX（注意：需註冊 KKTIX 帳號，後續才能進行修改）</li>
+<li>開始練習 <a href="https://www.freecodecamp.org/">freeCodeCamp</a> 線上課程</li>
+<ul>
+<li>HTML5 and CSS</li>
+<li>Basic JavaScript</li>
+<li>Git</li>
+<li>Node.js and Express.js</li>
+</ul>
+<li>加入社團，遇到問題可以在<a href="https://www.facebook.com/groups/293538921163334">社團</a>裡面討論</li>
+<li>完成練習，更新 kktix 資料</li>
+<li>等待第二階段通知</li>
+</ol>
+</v-card-text>
+</v-card>
+</v-layout>
+</v-parallax>
+</section> -->
+</div>
 </template>
